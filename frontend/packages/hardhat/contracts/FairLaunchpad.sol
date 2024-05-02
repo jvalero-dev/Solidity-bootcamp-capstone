4f6d9fa7982265d09524bd30bfc4db81232e3ff6
jvalero.dev <jvalero84.dev@gmail.com>
2024-05-02 12:15:02 +0200

    Adding events to launchap contract

--- frontend/packages/hardhat/contracts/FairLaunchpad.sol
+++ frontend/packages/hardhat/contracts/FairLaunchpad.sol
@@ -6,128 +6,177 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract FairLaunchpad is Ownable {
-    using SafeMath for uint256;
+	using SafeMath for uint256;
 
-    bool public started;
+	event LaunchpadStarted();
 
-    uint256 public investmentAmount;
-    uint256 public rewardAmount;
+	event InvestorJoined(address indexed investor);
 
-    uint256 public maxInvestors;
-    uint256 public endBlock;
+	event InvestorClaimedTokens(address indexed investor, uint256 amount);
 
-    bool public winnersChosen;
-    bool public projectRedeemed;
+	event InvestorRecoveredInvestment(
+		address indexed investor,
+		uint256 investmentAmount
+	);
 
-    mapping(address => bool) public invested;
-    mapping(address => bool) public isChosen;
+	event OwnerRedeemsProject(address indexed owner);
 
-    address[] private candidates;
+	event WinnersChosen(uint256 investors, uint256 candidates);
 
-    IERC20 public projectToken;
+	bool public started;
 
-    constructor(
-        uint256 _investmentAmount,
-        uint256 _rewardAmount,
-        uint256 _maxInvestors,
-        IERC20 _projectToken
-    ) {
-        investmentAmount = _investmentAmount;
-        rewardAmount = _rewardAmount;
-        maxInvestors = _maxInvestors;
-        winnersChosen = false;
-        projectRedeemed = false;
-        projectToken = _projectToken;
+	uint256 public investmentAmount;
+	uint256 public rewardAmount;
 
-        started = false;
-    }
+	uint256 public maxInvestors;
+	uint256 public endBlock;
 
-    modifier postStarted() {
-        require(started, "Launchpad not initialized");
-        _;
-    }
+	bool public winnersChosen;
+	bool public projectRedeemed;
 
-    modifier postWinnersChosen() {
-        require(winnersChosen, "Winners not chosen yet");
-        _;
-    }
+	mapping(address => bool) public invested;
+	mapping(address => bool) public isChosen;
 
-    function start(uint256 _durationBlocks) external onlyOwner {
-        require(!started, "Already started");
-        started = true;
-        endBlock = block.number + _durationBlocks;
-        require(
-            projectToken.transferFrom(msg.sender, address(this), rewardAmount.mul(maxInvestors)),
-            "Token transfer failed"
-        );
-    }
+	address[] private candidates;
 
+	IERC20 public projectToken;
 
-    function join() external payable postStarted {
-        require(block.number < endBlock, "Investment round is over");
-        require(msg.value == investmentAmount, "Invalid investment amount");
-        require(!invested[msg.sender], "Already joined");
+	constructor(
+		uint256 _investmentAmount,
+		uint256 _rewardAmount,
+		uint256 _maxInvestors,
+		IERC20 _projectToken
+	) Ownable() {
+		investmentAmount = _investmentAmount;
+		rewardAmount = _rewardAmount;
+		maxInvestors = _maxInvestors;
+		winnersChosen = false;
+		projectRedeemed = false;
+		projectToken = _projectToken;
 
-        invested[msg.sender] = true;
-        candidates.push(msg.sender);
-    }
+		started = false;
+	}
 
-    function chooseWinners() external postStarted {
-        require(block.number >= endBlock + 100, "Too early to choose winners");
-        require(!winnersChosen, "Winners already chosen");
-        require(candidates.length > 0, "No candidates to choose from");
+	modifier postStarted() {
+		require(started, "Launchpad not initialized");
+		_;
+	}
 
-        winnersChosen = true;
+	modifier postWinnersChosen() {
+		require(winnersChosen, "Winners not chosen yet");
+		_;
+	}
 
-        address[] memory tempCandidates = candidates;
-        uint256 candidateCount = tempCandidates.length;
-        uint256 randomSeed = uint256(block.prevrandao);
+	function start(uint256 _durationBlocks) external onlyOwner {
+		require(!started, "Already started");
+		started = true;
+		endBlock = block.number + _durationBlocks;
+		require(
+			projectToken.transferFrom(
+				msg.sender,
+				address(this),
+				rewardAmount.mul(maxInvestors)
+			),
+			"Token transfer failed"
+		);
+		emit LaunchpadStarted();
+	}
 
-        for (uint256 i = 0; i < maxInvestors && candidateCount > 0; i++) {
-            uint256 idx = uint256(keccak256(abi.encode(randomSeed, i))) % candidateCount;
-            address winner = tempCandidates[idx];
+	function join() external payable postStarted {
+		require(block.number < endBlock, "Investment round is over");
+		require(msg.value == investmentAmount, "Invalid investment amount");
+		require(!invested[msg.sender], "Already joined");
 
-            isChosen[winner] = true;
+		invested[msg.sender] = true;
+		candidates.push(msg.sender);
+		emit InvestorJoined(msg.sender);
+	}
 
-            tempCandidates[idx] = tempCandidates[candidateCount - 1];
-            candidateCount--;
-        }
-    }
+	function chooseWinners() external postStarted {
+		require(block.number >= endBlock + 100, "Too early to choose winners");
+		require(!winnersChosen, "Winners already chosen");
+		require(candidates.length > 0, "No candidates to choose from");
 
-    function redeem() external postWinnersChosen {
-        require(invested[msg.sender], "No investments made or already redeemed");
+		winnersChosen = true;
 
-        invested[msg.sender] = false;
+		address[] memory tempCandidates = candidates;
+		uint256 candidateCount = tempCandidates.length;
+		uint256 randomSeed = uint256(block.prevrandao);
 
-        if (isChosen[msg.sender]) {
-            require(projectToken.transfer(msg.sender, rewardAmount), "Token transfer failed");
-        } else {
-            require(payable(msg.sender).send(investmentAmount), "Refund failed");
-        }
-    }
+		for (uint256 i = 0; i < maxInvestors && candidateCount > 0; i++) {
+			uint256 idx = uint256(keccak256(abi.encode(randomSeed, i))) %
+				candidateCount;
+			address winner = tempCandidates[idx];
 
-    function projectRedeem() external onlyOwner postWinnersChosen {
-        require(winnersChosen, "Redeeming not possible before choosing winners");
-        require(!projectRedeemed, "Already redeemed");
+			isChosen[winner] = true;
 
-        projectRedeemed = true;
+			tempCandidates[idx] = tempCandidates[candidateCount - 1];
+			candidateCount--;
+		}
+		emit WinnersChosen(maxInvestors, candidates.length);
+	}
 
-        if (candidates.length < maxInvestors) {
-            require(
-                projectToken.transfer(
-                    msg.sender,
-                    (maxInvestors - candidates.length) * rewardAmount
-                ),
-                "Token transfer failed"
-            );
-        }
+	function redeem() external postWinnersChosen {
+		require(
+			invested[msg.sender],
+			"No investments made or already redeemed"
+		);
 
-        if (candidates.length > 0) {
-            uint256 investorsCount = maxInvestors <= candidates.length
-                ? maxInvestors
-                : candidates.length;
+		invested[msg.sender] = false;
 
-            require(payable(msg.sender).send(investorsCount * investmentAmount), "Refund failed");
-        }
-    }
+		if (isChosen[msg.sender]) {
+			require(
+				projectToken.transfer(msg.sender, rewardAmount),
+				"Token transfer failed"
+			);
+			emit InvestorClaimedTokens(msg.sender, rewardAmount);
+		} else {
+			require(
+				payable(msg.sender).send(investmentAmount),
+				"Refund failed"
+			);
+			emit InvestorRecoveredInvestment(msg.sender, investmentAmount);
+		}
+	}
+
+	function projectRedeem() external onlyOwner postWinnersChosen {
+		require(
+			winnersChosen,
+			"Redeeming not possible before choosing winners"
+		);
+		require(!projectRedeemed, "Already redeemed");
+
+		projectRedeemed = true;
+
+		if (candidates.length < maxInvestors) {
+			require(
+				projectToken.transfer(
+					msg.sender,
+					(maxInvestors - candidates.length) * rewardAmount
+				),
+				"Token transfer failed"
+			);
+		}
+
+		if (candidates.length > 0) {
+			uint256 investorsCount = maxInvestors <= candidates.length
+				? maxInvestors
+				: candidates.length;
+
+			require(
+				payable(msg.sender).send(investorsCount * investmentAmount),
+				"Refund failed"
+			);
+		}
+
+		emit OwnerRedeemsProject(msg.sender);
+	}
+
+	function getCandidatesCount()
+		public
+		view
+		returns (uint256 candidatesNumber)
+	{
+		candidatesNumber = candidates.length;
+	}
 }
