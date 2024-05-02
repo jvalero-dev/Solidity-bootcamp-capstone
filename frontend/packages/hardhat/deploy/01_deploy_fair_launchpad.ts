4873531717a5e41a4020e30e3a1dcc3c33341aca
jvalero.dev <jvalero84.dev@gmail.com>
2024-05-02 12:44:44 +0200

    Launchpad start triggered by admin, removing from deployment script.

--- frontend/packages/hardhat/deploy/01_deploy_fair_launchpad.ts
+++ frontend/packages/hardhat/deploy/01_deploy_fair_launchpad.ts
@@ -4,45 +4,39 @@ import { parseEther } from "ethers";
 import fs from "fs";
 import { ethers } from "hardhat";
 
-
 const deployLaunchpad: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployer } = await hre.getNamedAccounts();
-    const { deploy } = hre.deployments;
- 
-    const tokenAddress = fs.readFileSync("tokenAddress.txt", "utf-8");
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
 
-    const tokenContract = await ethers.getContractAt("MyToken", tokenAddress);
-    const deployerBalance = await tokenContract.balanceOf(deployer);
-    console.log("Deployer Token Balance:", deployerBalance.toString());
- 
-    const investmentAmount = parseEther('0.1'); // Ether investment amount for Launchpad in wei
-    const rewardAmount = parseEther('10'); // Reward amount in tokens in wei
-    const maxInvestors = 5; // Maximum number of investors for the Launchpad
-    const durationBlocks = 100; // Duration in blocks for the Launchpad investment round
- 
-    const launchpadDeployment = await deploy("FairLaunchpad", {
-        from: deployer,
-        args: [
-          investmentAmount,
-          rewardAmount,
-          maxInvestors,
-          tokenAddress
-        ],
-        log: true,
-        autoMine: true,  
-    });
-    const launchpadAddress = launchpadDeployment.address;
-    const launchpadContract = await ethers.getContractAt("FairLaunchpad", launchpadAddress);
+  const tokenAddress = fs.readFileSync("tokenAddress.txt", "utf-8");
 
-    await tokenContract.approve(launchpadAddress, rewardAmount * BigInt(maxInvestors))
-    
-    await launchpadContract.start(durationBlocks);
+  const tokenContract = await ethers.getContractAt("MyToken", tokenAddress);
+  const deployerBalance = await tokenContract.balanceOf(deployer);
+  console.log("Deployer Token Balance:", deployerBalance.toString());
 
-    const launchpadBalance = await tokenContract.balanceOf(launchpadAddress);
-    console.log("Launchpad Token Balance:", launchpadBalance.toString());
+  const investmentAmount = parseEther("0.1"); // Ether investment amount for Launchpad in wei
+  const rewardAmount = parseEther("10"); // Reward amount in tokens in wei
+  const maxInvestors = 5; // Maximum number of investors for the Launchpad
+  const durationBlocks = 100; // Duration in blocks for the Launchpad investment round
 
-    fs.unlinkSync("tokenAddress.txt");
-}
+  const launchpadDeployment = await deploy("FairLaunchpad", {
+    from: deployer,
+    args: [investmentAmount, rewardAmount, maxInvestors, tokenAddress],
+    log: true,
+    autoMine: true,
+  });
+  const launchpadAddress = launchpadDeployment.address;
+  const launchpadContract = await ethers.getContractAt("FairLaunchpad", launchpadAddress);
+
+  await tokenContract.approve(launchpadAddress, rewardAmount * BigInt(maxInvestors));
+
+  //await launchpadContract.start(durationBlocks);
+
+  const launchpadBalance = await tokenContract.balanceOf(launchpadAddress);
+  console.log("Launchpad Token Balance:", launchpadBalance.toString());
+
+  fs.unlinkSync("tokenAddress.txt");
+};
 
 export default deployLaunchpad;
 
